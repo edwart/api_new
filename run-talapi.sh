f803dc381174e8b823c43c30d6057f59c4e00ad0
Peter Edwards <peter@dragonstaff.co.uk>
2016-12-05 14:49:36 +0000

    add staging environment and use for QA testing by keyapps

--- run-talapi.sh
+++ run-talapi.sh
@@ -2,5 +2,6 @@
 
 #--env development
 #--env production
-plackup --server HTTP::Server::Simple --port 5000 --host 127.0.0.1 --daemonize --access-log /var/www/cgi-bin/tal-api/plack.access.log bin/app.psgi
+#--daemonize
+plackup --server HTTP::Server::Simple --port 5000 --host 127.0.0.1 --env staging --access-log /var/www/cgi-bin/tal-api/plack.access.log bin/app.psgi &
 
