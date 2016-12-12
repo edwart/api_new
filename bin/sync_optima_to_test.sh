2b0845c5cc82d6b6e2e4c6ed1a8639fa793de5ed
Peter Edwards <peter@dragonstaff.co.uk>
2016-12-12 11:30:43 +0000

    skip cands to avoid overwrite fixture records

--- bin/sync_optima_to_test.sh
+++ bin/sync_optima_to_test.sh
@@ -4,6 +4,7 @@
 #mysqlhotcopy --dryrun Optima Optima_test --user=root --password=gemini2 --addtodest
 
 # skip document and accesslog tables as they are huge and not needed
+# cands has a couple of fixture records not to overwrite
 #mysqlhotcopy --dryrun 'Optima./~(document|accesslog).*/' Optima_test --user=root --password=gemini2 --addtodest
-mysqlhotcopy 'Optima./~(document|accesslog).*/' Optima_test --user=beacon --password=saturn5 --addtodest
+mysqlhotcopy 'Optima./~(document|accesslog|cands).*/' Optima_test --user=beacon --password=saturn5 --addtodest
 
