9f0d1a04420af03b01bb94afdebdc689af3a2c5e
Peter Edwards <peter@dragonstaff.co.uk>
2017-02-14 14:35:02 +0000

    put log in sensible place

--- run-talapi.sh
+++ run-talapi.sh
@@ -3,5 +3,5 @@
 #--env development
 #--env production
 #--daemonize
-plackup --server HTTP::Server::Simple --port 5000 --host 127.0.0.1 --env staging --access-log /var/www/cgi-bin/tal-api/plack.access.log bin/app.psgi &
+nohup plackup --server HTTP::Server::Simple --port 5000 --host 127.0.0.1 --env staging --access-log /var/www/cgi-bin/tal-api/logs/plack.access.log bin/app.psgi &
 
