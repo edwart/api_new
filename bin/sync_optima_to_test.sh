ddd3d67fc62d8f72b5b1e00603e2e3eb27707823
Peter Edwards <peter@dragonstaff.co.uk>
2017-02-14 15:15:19 +0000

    recreate /var/lib/mysql/Optima_test symlink is mysqlhotcopy removes it on file copy error

--- bin/sync_optima_to_test.sh
+++ bin/sync_optima_to_test.sh
@@ -6,5 +6,7 @@
 # skip document and accesslog tables as they are huge and not needed
 # cands has a couple of fixture records not to overwrite
 #mysqlhotcopy --dryrun 'Optima./~(document|accesslog).*/' Optima_test --user=root --password=gemini2 --addtodest
-mysqlhotcopy 'Optima./~(document|accesslog|cands).*/' Optima_test --user=beacon --password=saturn5 --addtodest
+mysqlhotcopy 'Optima./~(document|accesslog|cands).*/' Optima_test --user=beacon --password=saturn5 --addtodest --keepold
+
+[ ! -h /var/lib/mysql/Optima_test ] && ln -s /usr2/mysqldata/Optima_test /var/lib/mysql/Optima_test
 
