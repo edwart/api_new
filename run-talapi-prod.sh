dc4b714987b7f592a463ccbfa20825e3f5718dd8
Tony Edwardson <tonye@talisman.tech>
2017-09-10 12:31:28 +0100

    cleanup

--- run-talapi-prod.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#--env development
-#--env production
-#--daemonize
-nohup plackup --server HTTP::Server::Simple --port 5001 --host 127.0.0.1 --env production --access-log /var/www/cgi-bin/tal-api/logs/plack-prod.access.log bin/app.psgi &
-
--- run-talapi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#--env development
-#--env production
-#--daemonize
-> logs/server.log
-start_server --port=5000 --daemonize --log-file=logs/server.log --status-file=logs/server.status --pid-file=logs/server.pid -- /usr/local/bin/plackup -s Starman bin/app.psgi
-#nohup plackup --server HTTP::Server::Simple --port 5000 --host 127.0.0.1 --env staging --access-log logs/plack.access.log bin/app.psgi &
-
