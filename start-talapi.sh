173d6fb35bd3bf70bf7ef2682956692b14e35001
Tony Edwardson <tonye@talisman.tech>
2017-09-11 11:16:00 +0100

    bug fix

--- start-talapi.sh
+++ start-talapi.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 port=5000
+mkdir -p logs
 > logs/server.log
 start_server --port=$port --daemonize --log-file=$PWD/logs/server.log --status-file=$PWD/logs/server.status --pid-file=$PWD/logs/server.pid -- /usr/local/bin/plackup -p $port -s Starman bin/app.psgi
 
